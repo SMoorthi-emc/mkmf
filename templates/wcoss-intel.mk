7375c5424ad0c106bb6f965eff686a4b86116a2f
jiandewang <jiande.wang@noaa.gov>
2018-03-19 22:35:24 -0400

    add wcoss-intel.mk and modified theia-intel.mk

--- templates/wcoss-intel.mk
+++ templates/wcoss-intel.mk
@@ -27,13 +27,15 @@ FPPFLAGS := -fpp -Wp,-w
 # NESCC systems set HDF5 as the root directory to the HDF5
 # development libraries.
 #HDF5=/apps/hdf5/1.8.14-intel-impi  <-- can compile with this, run with 1 PE, but not >1 PE
-HDF5=/apps/hdf5/1.8.14-intel
-INC = -I$(HDF5)/include $(shell nf-config --fflags)
+#HDF5=/apps/hdf5/1.8.14-intel
+HDF5=/usrx/local/HDF5/1.8.9/serial
+
+# INC = -I$(HDF5)/include $(shell nf-config --fflags)
+INC = -I$(HDF5)/include
 FFLAGS := -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -i4 -r8 -nowarn -sox -traceback $(INC)
 FFLAGS_OPT = -O3 -debug minimal -fp-model source -override-limits
 FFLAGS_DEBUG = -g -O0 -check -check noarg_temp_created -check nopointer -warn -warn noerrors -fpe0 -ftrapuv
 FFLAGS_DEBUG = -g -O0 -traceback 
-FFLAGS_DEBUG = -g -O0 -heap-arrays -traceback -check bounds -check -check noarg_temp_created -check nopointer -check all -fp-stack-check -warn -warn noerrors -fpe0 -ftrapuv
 FFLAGS_REPRO = -O2 -debug minimal -fp-model source -override-limits
 FFLAGS_OPENMP = -openmp
 FFLAGS_VERBOSE = -v -V -what
@@ -85,6 +87,9 @@ ifneq ($(findstring netcdf/4,$(LOADEDMODULES)),)
 else
   LIBS += -lnetcdf
 endif
+
+FFLAGS += -I/usrx/local/NetCDF/4.2/serial/include
+CFLAGS += -I/usrx/local/NetCDF/4.2/serial/include
 
 LIBS +=
 LIBS += -lmkl_blas95_lp64 -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential
